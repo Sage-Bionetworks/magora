3921614e8af963026111522b03c9d40b9ec0f0bf
Sharla Gelfand <sharla.gelfand@gmail.com>
2020-09-09 19:38:27 -0400

    Add tissue for filtering, increase plot size based on number of mouse lines selected

--- R/mod_gene_expression.R
+++ R/mod_gene_expression.R
@@ -7,6 +7,7 @@
 #' @noRd
 mod_gene_expression_ui <- function(id) {
   ns <- shiny::NS(id)
+
   shiny::tabPanel(
     "Gene Expression",
     shiny::sidebarLayout(
@@ -15,27 +16,24 @@ mod_gene_expression_ui <- function(id) {
         shiny::selectInput(
           ns("gene"),
           "Select a gene",
-          choices = unique(magora::gene_expressions[["gene"]]),
+          choices = sort(magora::gene_expressions_genes),
           multiple = FALSE,
           selectize = TRUE
         ),
         shinyWidgets::pickerInput(
           ns("mouse_line"),
           "Select mouse lines",
-          choices = as.character(levels(magora::gene_expressions[["mouse_line"]])),
+          choices = sort(magora::gene_expressions_mouse_lines),
           multiple = TRUE,
           selected = c("5XFAD", "C57BL6J")
         ),
         shinyWidgets::pickerInput(
           ns("tissue"),
           "Select tissue",
-          choices = NULL
+          choices = sort(magora::gene_expressions_tissue)
         )
       ),
-      shiny::mainPanel(
-        width = 9,
-        shiny::plotOutput(ns("gene_expression_plot"))
-      )
+      shiny::uiOutput(ns("gene_expression_plot_ui"))
     )
   )
 }
@@ -46,15 +44,18 @@ mod_gene_expression_ui <- function(id) {
 mod_gene_expression_server <- function(input, output, session) {
   ns <- session$ns
 
+  gene_expressions <- golem::get_golem_options("gene_expressions")
+
   filtered_gene_expressions <- shiny::reactive({
     shiny::validate(
       shiny::need(!is.null(input$mouse_line), message = "Please select one or more mouse lines.")
     )
 
-    magora::gene_expressions %>%
+    gene_expressions %>%
       dplyr::filter(
         .data$gene %in% input$gene,
-        .data$mouse_line %in% input$mouse_line
+        .data$mouse_line %in% input$mouse_line,
+        .data$tissue %in% input$tissue
       )
   })
 
@@ -66,6 +67,15 @@ mod_gene_expression_server <- function(input, output, session) {
     filtered_gene_expressions() %>%
       expand_mouse_line_factor_from_selection(input$mouse_line) %>%
       magora_boxplot(plot_type = "gene expression")
+  })
+
+  output$gene_expression_plot_ui <- shiny::renderUI({
+    shiny::mainPanel(
+      width = 9,
+      shiny::plotOutput(ns("gene_expression_plot"),
+        height = glue::glue("{ceiling(length(input$mouse_line)/2)*400}px")
+      )
+    )
   })
 }
 
