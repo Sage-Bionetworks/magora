88ca4a4859edd1ff2bdddb88ed373ae829d47797
Sharla Gelfand <sharla.gelfand@gmail.com>
2020-11-20 10:01:45 -0500

    Update data loading and filtering to handle parquet, use consistent widget for selecting gene

--- R/run_app.R
+++ R/run_app.R
@@ -4,13 +4,13 @@
 #'
 #' @export
 run_app <- function(
-                    ...) {
+  ...) {
   golem::with_golem_options(
     app = shiny::shinyApp(
       ui = app_ui,
       server = app_server
     ),
     golem_opts = list(... ,
-                      gene_expressions = readRDS(system.file("extdata/gene_expressions.rds", package = "magora")))
+                      gene_expressions = arrow::open_dataset(here::here("inst", "extdata", "gene_expressions")))
   )
 }
