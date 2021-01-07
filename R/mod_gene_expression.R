80bcc45533414395a049b9a7c7b582240434fe20
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-01-07 14:13:15 -0500

    WIP download gene expressions

--- R/mod_gene_expression.R
+++ R/mod_gene_expression.R
@@ -20,7 +20,7 @@ mod_gene_expression_ui <- function(id) {
       class = "magora-page",
         shiny::fluidRow(
           shiny::column(
-            width = 4,
+            width = 3,
             shinyWidgets::pickerInput(
               ns("gene"),
               "Gene",
@@ -30,7 +30,7 @@ mod_gene_expression_ui <- function(id) {
             )
           ),
           shiny::column(
-            width = 4,
+            width = 3,
             shinyWidgets::pickerInput(
               ns("mouse_line"),
               "Mouse lines",
@@ -40,12 +40,16 @@ mod_gene_expression_ui <- function(id) {
             )
           ),
           shiny::column(
-            width = 4,
+            width = 3,
             shinyWidgets::pickerInput(
               ns("tissue"),
               "Tissue",
               choices = magora::gene_expression_tissues
           )
+        ),
+        shiny::column(
+          width = 3,
+          shiny::downloadButton(ns("save_plot_data"), "Download plot and data")
         )
       ),
       shiny::column(
@@ -92,7 +96,7 @@ mod_gene_expression_server <- function(input, output, session, gene_expressions)
       dplyr::filter(mouse_line %in% input$mouse_line) # Arrow seems to have issue with %in%, so collect, then do the last filter
   })
 
-  output$gene_expression_plot <- shiny::renderPlot({
+  gene_expression_plot <- shiny::reactive({
     shiny::validate(
       shiny::need(nrow(filtered_gene_expressions()) > 0, message = "There is no data for the selected combination.")
     )
@@ -100,6 +104,10 @@ mod_gene_expression_server <- function(input, output, session, gene_expressions)
     filtered_gene_expressions() %>%
       expand_mouse_line_factor_from_selection(input$mouse_line) %>%
       magora_boxplot(plot_type = "gene expression")
+  })
+
+  output$gene_expression_plot <- shiny::renderPlot({
+    gene_expression_plot()
   })
 
   output$gene_expression_plot_ui <- shiny::renderUI({
@@ -115,6 +123,26 @@ mod_gene_expression_server <- function(input, output, session, gene_expressions)
     color = "#D3DCEF"
     )
   })
+
+  save_name <- reactive({
+    fs::path_sanitize(glue::glue("Gene_Expressions_{input$gene}_{paste0(input$mouse_line, collapse = '_')}_{input$tissue}"))
+  })
+
+  output$save_plot_data <- shiny::downloadHandler(
+    filename = function() {
+      glue::glue("{save_name()}.zip")
+    },
+    content = function(file) {
+      plot_file <- glue::glue("{save_name()}_plot.png")
+      ggplot2::ggsave(filename = plot_file, plot = gene_expression_plot(), width = 10, height = 5, units = "in", dpi = 300)
+
+      data_file <- glue::glue("{save_name()}_data.csv")
+
+      readr::write_csv(filtered_gene_expressions(), path = data_file)
+
+      zip(file, files = c(data_file, plot_file))
+    }
+  )
 }
 
 ## To be copied in the UI
