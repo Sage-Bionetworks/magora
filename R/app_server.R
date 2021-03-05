e1b66c3840115acd2aa229b488208247744c4389
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-03-05 13:27:56 -0500

    Remove old gene expression module, code, data

--- R/app_server.R
+++ R/app_server.R
@@ -5,9 +5,6 @@
 app_server <- function(input, output, session) {
   shiny::callModule(mod_pathology_server, "pathology")
 
-  gene_expressions <- golem::get_golem_options("gene_expressions")
-  shiny::callModule(mod_gene_expression_server, "gene_expression", gene_expressions)
-
   shiny::callModule(mod_gene_expression_volcano_server, "gene_expression_volcano")
 
   shiny::callModule(mod_gene_expression_volcano_plotly_server, "gene_expression_volcano_plotly")
--- R/data.R
+++ R/data.R
@@ -8,26 +8,6 @@
 #' A data set containing the tissues available for each mouse phenotype.
 "phenotype_tissue"
 
-#' Gene expressions genes
-#'
-#' A data set containing the genes in the gene expressions data.
-"gene_expression_genes"
-
-# Gene expressions mouse lines
-#'
-#' A data set containing the mouse lines in the gene expressions data.
-"gene_expression_mouse_lines"
-
-# Gene expressions tissues
-#'
-#' A data set containing the tissues in the gene expressions data.
-"gene_expression_tissues"
-
-#' Gene expressions mouse line tissues
-#'
-#' A data set containing the tissues available for each mouse line.
-"gene_expressions_mouse_line_tissues"
-
 #' Nanostring data
 #'
 #' A data set containing the correlation of nanostring log fold-change and AMP-AD human gene log fold-change, by module, model, sex, and age group.
