50642cb95f4355b6301cf2e173df406a01bd078c
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-05-01 14:17:10 -0400

    WIP sizing by pvalue

--- R/magora_heatmap.R
+++ R/magora_heatmap.R
@@ -1,11 +1,15 @@
 magora_heatmap <- function(data) {
 
+  # Set up p-value legend transformation
+  pvalue_legend_breaks <- c(0.001, 0.005, 0.01, 0.05, 0.1, 0.2)
+  log10_pvalue_legend_breaks <- -log10(pvalue_legend_breaks + 1)
+
   data %>%
     ggplot2::ggplot(ggplot2::aes(x = sex_age, y = gene)) +
     ggplot2::geom_tile(colour = "black", fill = "white") +
-    ggplot2::geom_point(ggplot2::aes(size = padj, fill = log2foldchange), shape = 21) +
+    ggplot2::geom_point(ggplot2::aes(size = -log10(padj + 1), fill = log2foldchange), shape = 21) +
     ggplot2::facet_grid(cols = dplyr::vars(strain)) +
-    ggplot2::scale_size(name = "Adjusted P-Value", trans = "reverse", limits = c(1, 0), range = c(1, 5)) +
+    ggplot2::scale_size(name = "Adjusted P-Value", limits = range(log10_pvalue_legend_breaks), breaks = log10_pvalue_legend_breaks, labels = pvalue_legend_breaks, range = c(1, 5)) +
     ggplot2::scale_fill_gradient2(low = "#85070C", high = "#164B6E", name = "Log 2 Fold change", limits = c(-1, 1), breaks = c(-1, 0, 1)) +
     ggplot2::guides(
       fill = ggplot2::guide_colourbar(title.position = "top", title.hjust = 0.5, ticks = FALSE),
--- tests/testthat/test-mod_download.R
+++ tests/testthat/test-mod_download.R
@@ -2,6 +2,9 @@ test_that("download_name constructs a string that uses the data type, and separa
   x <- download_name("phenotype", "x", c("y", "z"), "a/b")
   expect_equal(x, "Pathology_x_y_z_ab")
 
-  x <- download_name("gene_expression", "x", c("y", "z"), "a/b")
-  expect_equal(x, "Gene_Expression_x_y_z_ab")
+  x <- download_name("gene_expression_volcano", "x", c("y", "z"), "a/b")
+  expect_equal(x, "Gene_Expression_Volcano_x_y_z_ab")
+
+  x <- download_name("gene_expression_heatmap", "x", c("y", "z"), "a/b")
+  expect_equal(x, "Gene_Expression_Heatmap_x_y_z_ab")
 })
