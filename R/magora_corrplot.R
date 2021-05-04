bd75c3a5d6f9387cb42010b66e0fa9200ef11370
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-05-03 21:23:00 -0400

    Show all nanostring data, not just significant, and size by pvalue

--- R/magora_corrplot.R
+++ R/magora_corrplot.R
@@ -6,11 +6,23 @@
 #'
 #' @return A ggplot2 object.
 magora_corrplot <- function(data) {
-  ggplot2::ggplot() +
-    ggplot2::geom_tile(data = data, ggplot2::aes(x = .data$module, y = .data$model_sex), colour = "black", fill = "white") +
-    ggplot2::geom_point(data = dplyr::filter(data, .data$significant), ggplot2::aes(x = .data$module, y = .data$model_sex, colour = .data$correlation, size = abs(.data$correlation))) +
+
+  # Set up p-value legend transformation
+  pvalue_breaks <- c(0.001, 0.005, 0.01, 0.05, 0.1, 0.2)
+  log10_pvalue_breaks <- -log10(pvalue_breaks)
+
+  # Manually split p-values into categories to handle smaller than smallest and larger than largest
+  data <- categorize_pvalues(data, p_value, pvalue_breaks, log10_pvalue_breaks)
+
+  ggplot2::ggplot(data, ggplot2::aes(x = .data$module, y = .data$model_sex)) +
+    ggplot2::geom_tile(colour = "black", fill = "white") +
+    ggplot2::geom_point(ggplot2::aes(fill = .data$correlation, size = padj_category_log10), shape = 21) +
     ggplot2::scale_x_discrete(position = "top") +
-    ggplot2::scale_size(guide = "none", limits = c(0, 0.5)) +
+    ggplot2::scale_size(
+      name = "P-Value",
+      limits = range(log10_pvalue_breaks),
+      breaks = log10_pvalue_breaks, labels = pvalue_breaks, range = c(1, 6), guide = ggplot2::guide_legend()
+    ) +
     ggplot2::scale_color_gradient2(limits = c(-0.5, 0.5), breaks = c(-0.5, 0, 0.5), low = "#85070C", high = "#164B6E", name = "Correlation", guide = ggplot2::guide_colorbar(ticks = FALSE)) +
     ggplot2::labs(x = NULL, y = NULL) +
     ggplot2::facet_grid(rows = dplyr::vars(.data$age_group), cols = dplyr::vars(.data$cluster_label), scales = "free", space = "free") +
