30cc2ffc206bf2e60779fa60d46276a6423bc4ed
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-05-03 21:05:54 -0400

    Categorize p-values for legend

--- R/magora_heatmap.R
+++ R/magora_heatmap.R
@@ -1,15 +1,22 @@
 magora_heatmap <- function(data) {
 
   # Set up p-value legend transformation
-  pvalue_legend_breaks <- c(0.001, 0.005, 0.01, 0.05, 0.1, 0.2)
-  log10_pvalue_legend_breaks <- -log10(pvalue_legend_breaks + 1)
+  pvalue_breaks <- c(0.001, 0.005, 0.01, 0.05, 0.1, 0.2)
+  log10_pvalue_breaks <- -log10(pvalue_breaks)
+
+  # Manually split p-values into categories to handle smaller than smallest and larger than largest
+  data <- categorize_pvalues(data, padj, pvalue_breaks, log10_pvalue_breaks)
 
   data %>%
     ggplot2::ggplot(ggplot2::aes(x = sex_age, y = gene)) +
     ggplot2::geom_tile(colour = "black", fill = "white") +
-    ggplot2::geom_point(ggplot2::aes(size = -log10(padj + 1), fill = log2foldchange), shape = 21) +
+    ggplot2::geom_point(ggplot2::aes(size = padj_category_log10, fill = log2foldchange), shape = 21) +
     ggplot2::facet_grid(cols = dplyr::vars(strain)) +
-    ggplot2::scale_size(name = "Adjusted P-Value", limits = range(log10_pvalue_legend_breaks), breaks = log10_pvalue_legend_breaks, labels = pvalue_legend_breaks, range = c(1, 5)) +
+    ggplot2::scale_size(
+      name = "Adjusted P-Value",
+      limits = range(log10_pvalue_breaks),
+      breaks = log10_pvalue_breaks, labels = pvalue_breaks, range = c(1, 6), guide = ggplot2::guide_legend()
+    ) +
     ggplot2::scale_fill_gradient2(low = "#85070C", high = "#164B6E", name = "Log 2 Fold change", limits = c(-1, 1), breaks = c(-1, 0, 1)) +
     ggplot2::guides(
       fill = ggplot2::guide_colourbar(title.position = "top", title.hjust = 0.5, ticks = FALSE),
@@ -23,5 +30,19 @@ magora_heatmap <- function(data) {
       legend.position = "bottom",
       panel.background = ggplot2::element_blank(),
       panel.grid = ggplot2::element_blank()
+    )
+}
+
+categorize_pvalues <- function(data, pvalue, pvalue_breaks, log10_pvalue_breaks) {
+  data %>%
+    dplyr::mutate(
+      padj_category = cut({{ pvalue }}, pvalue_breaks, labels = FALSE),
+      padj_category = dplyr::case_when(
+        !is.na(padj_category) ~ padj_category,
+        {{ pvalue }} < min(pvalue_breaks) ~ 1L,
+        {{ pvalue }} > max(pvalue_breaks) ~ length(pvalue_breaks)
+      ),
+      padj_category = purrr::map_dbl(padj_category, ~ ifelse(is.na(.x), NA_real_, pvalue_breaks[[.x]])),
+      padj_category_log10 = -log10(padj_category)
     )
 }
