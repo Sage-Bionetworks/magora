d49a42ae14c00cbd7c29827edee4ce4d717b34fa
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-06-15 15:33:03 -0400

    Move legend to be on two rows

--- R/magora_volcano_plot.R
+++ R/magora_volcano_plot.R
@@ -26,7 +26,7 @@ magora_volcano_plot <- function(data, data_labels, type = "ggplot2", facet = TRU
   # Create data for threshold lines
   # Need to get a bit creative with this, because combining vline and hline legends in ggplot creates a "crossed" legend which is unappealing - to have a legend that is just horizontal, add "dummy" fold change data to the p_value data frame, then set the real fold change line to have the same linetype as that
   fold_change_line <- dplyr::tibble(x = c(-1, 1))
-  p_value_line <- dplyr::tibble(y = -log10(0.05), label = "P-value = 0.05") %>%
+  p_value_line <- dplyr::tibble(y = -log10(0.05), label = "Adjusted P-value = 0.05") %>%
     dplyr::mutate(label = forcats::fct_expand(label, "Log2 Fold Change = -1, 1")) %>%
     tidyr::complete(label)
 
@@ -46,14 +46,17 @@ magora_volcano_plot <- function(data, data_labels, type = "ggplot2", facet = TRU
 
   # Create plot
   p <- ggplot2::ggplot() +
-    ggplot2::geom_point(data = data, ggplot2::aes(x = .data$log2foldchange, y = .data$neg_log10_padj, colour = .data$diff_expressed, text = .data$gene), alpha = 0.25) +
+    ggplot2::geom_point(data = dplyr::filter(data, !is.na(neg_log10_padj)), ggplot2::aes(x = .data$log2foldchange, y = .data$neg_log10_padj, colour = .data$diff_expressed, text = .data$gene), alpha = 0.25) +
     ggplot2::geom_vline(data = fold_change_line, ggplot2::aes(xintercept = x), linetype = "dashed") +
     ggplot2::geom_hline(data = p_value_line, ggplot2::aes(yintercept = y, linetype = label)) +
     ggplot2::scale_colour_manual(values = c("#85070C", "darkgrey", "#164B6E"), name = NULL, guide = ggplot2::guide_legend(override.aes = list(size = 3), order = 1), drop = FALSE) +
     ggplot2::scale_linetype_discrete(guide = ggplot2::guide_legend(reverse = TRUE, order = 2), name = NULL) +
     sagethemes::theme_sage() +
     ggplot2::coord_cartesian(clip = "off") +
-    ggplot2::theme(legend.position = "top")
+    ggplot2::theme(
+      legend.position = "top",
+      legend.box = "vertical"
+    )
 
   if (facet) {
     p <- p +
@@ -84,7 +87,6 @@ magora_volcano_plot <- function(data, data_labels, type = "ggplot2", facet = TRU
     }
 
     p
-
   }
 }
 
