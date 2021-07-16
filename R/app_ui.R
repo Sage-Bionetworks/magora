ab70e57edb4b63cbff1c4d5bd1838b7354a4481c
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-07-16 13:49:25 -0400

    Remove line about Ab data in pathology page, since currently not present because of metadata issues

--- R/app_ui.R
+++ R/app_ui.R
@@ -41,7 +41,7 @@ golem_add_external_resources <- function() {
     golem::favicon(ext = "png"),
     golem::bundle_resources(
       path = app_sys("app/www"),
-      app_title = "AD Model Explorer"
+      app_title = "MODEL-AD Explorer"
     )
   )
 }
