a5821785550512c9890241f3468779e2a1e0b4fb
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-02-28 16:17:23 -0500

    Start parsing of 3xTg model

--- data-raw/phenotypes.R
+++ data-raw/phenotypes.R
@@ -177,6 +177,194 @@ phenotypes <- phenotypes %>%
   ) %>%
   arrange(phenotype)
 
+# 3xTg ----
+
+## Download and read  data ----
+
+# Read file that contains the phenotype name, synapse ID, field to use, and values of Stain to filter for
+phenotype_files <- read_csv(here::here("data-raw", "pathology", "3xTg", "3xTg_data_sources.csv"))
+
+# Check that the latest version of all files is used
+walk2(phenotype_files[["syn_id"]], phenotype_files[["version"]], check_latest_version)
+
+# Download data - synapser checks the version locally and does not redownload if the version is up to date, so we can safely run all of this without worrying about redownloading
+
+phenotype_distinct_files <- phenotype_files %>%
+  distinct(syn_id, version)
+
+phenotype_paths <- map2(phenotype_distinct_files[["syn_id"]], phenotype_distinct_files[["version"]], ~ synapser::synGet(.x, version = .y, downloadLocation = here::here("data-raw", "pathology", "3xTg"), ifcollision = "overwrite.local"))
+
+# Extract path and combine with phenotype_files df so that full path can be used, in case file name changes, rather than whatever is hardcoded in
+
+phenotype_paths <- phenotype_paths %>%
+  map("path") %>%
+  map(~ tibble(file = .x))
+
+names(phenotype_paths) <- phenotype_distinct_files[["syn_id"]]
+
+phenotype_paths <- phenotype_paths %>%
+  bind_rows(.id = "syn_id")
+
+phenotype_files <- phenotype_files %>%
+  left_join(phenotype_paths, by = "syn_id", suffix = c("_hardcoded", ""))
+
+# Function to read in, filter + clean data, and return relevant fields
+read_clean_phenotype <- function(field, stain_filter, file) {
+  res <- readr::read_csv(file, na = c("N/A", ""))
+
+  res <- res %>%
+    janitor::clean_names()
+
+  if (!is.na(stain_filter)) {
+    res <- res %>%
+      dplyr::filter(stain == stain_filter)
+  }
+
+  res <- res %>%
+    # Some will not have individual_id because they have a Pool identifier instead - that's okay, just grab it if it is there
+    dplyr::select(tidyselect::any_of(c("individual_id")), tidyselect::all_of(c("specimen_id", value = janitor::make_clean_names(field)))) %>%
+    dplyr::mutate_all(as.character) %>%
+    janitor::remove_empty("rows")
+
+  # If it's not there, add a "fake" one just for keeping track of some ID
+
+  if (!"individual_id" %in% names(res)) {
+    res <- res %>%
+      mutate(individual_id = as.character(row_number()))
+  }
+
+  res
+}
+
+# Read in data
+
+phenotype_data <- phenotype_files %>%
+  mutate(data = pmap(list(field, stain_filter, file), read_clean_phenotype)) %>%
+  select(syn_id, phenotype, units, data) %>%
+  unnest(cols = data) %>%
+  mutate(value = as.numeric(value)) %>%
+  filter(!is.na(value))
+
+## Metadata ----
+
+### Biospecimen metadata ----
+
+biospecimen_id <- "syn23532198"
+biospecimen_version <- 3
+
+check_latest_version(biospecimen_id, biospecimen_version)
+
+biospecimen_metadata_path <- synGet(biospecimen_id, version = biospecimen_version, downloadLocation = here::here("data-raw", "pathology", "3xTg"), ifcollision = "overwrite.local")
+
+biospecimen_metadata <- read_csv(biospecimen_metadata_path[["path"]]) %>%
+  mutate(individualID = as.character(individualID)) %>%
+  clean_names() %>%
+  select(individual_id, specimen_id, tissue)
+
+### Individual metadata ----
+
+individual_id <- "syn23532199"
+individual_version <- 4
+
+check_latest_version(individual_id, individual_version)
+
+individual_metadata_path <- synGet(individual_id, version = individual_version, downloadLocation = here::here("data-raw", "pathology", "3xTg"), ifcollision = "overwrite.local")
+
+individual_metadata <- read_csv(individual_metadata_path[["path"]]) %>%
+  mutate(individualID = as.character(individualID)) %>%
+  clean_names() %>%
+  select(individual_id, sex, genotype, genotype_background, individual_common_genotype, age_death, age_death_units)
+
+### Check missing IDs ----
+
+# Check which IDs are missing from metadata
+phenotype_data %>%
+  filter(!str_starts(specimen_id, "Pool")) %>%
+  anti_join(biospecimen_metadata, by = c("individual_id", "specimen_id")) %>%
+  distinct(individual_id, specimen_id)
+
+phenotype_data %>%
+  filter(!str_starts(specimen_id, "Pool")) %>%
+  anti_join(individual_metadata, by = "individual_id") %>%
+  distinct(individual_id)
+
+## Clean data ----
+
+biospecimen_metadata <- biospecimen_metadata %>%
+  mutate(tissue = str_to_title(tissue))
+
+# Check that all age death units are "months"
+
+individual_metadata %>%
+  count(age_death_units) %>%
+  pull(age_death_units) == "months"
+
+# Check ages
+
+individual_metadata %>%
+  count(age_death)
+
+## Derive features
+
+individual_metadata <- individual_metadata %>%
+  mutate(
+    sex = str_to_title(sex),
+    sex = as_factor(sex),
+    age_factor = as_factor(age_death),
+    age_factor = fct_reorder(age_factor, age_death),
+    mouse_model = individual_common_genotype
+  ) %>%
+  select(-genotype, -genotype_background) %>%
+  rename(age = age_factor)
+
+### Form metadata for Pool identified specimens ----
+
+pool_biospecimen_metadata <- biospecimen_metadata %>%
+  filter(str_starts(specimen_id, "Pool"))
+
+pool_metadata <- pool_biospecimen_metadata %>%
+  left_join(individual_metadata, by = "individual_id") %>%
+  select(-individual_id) %>%
+  distinct() %>%
+  arrange(specimen_id)
+
+# Check that there is only one row for each pool
+
+pool_metadata %>%
+  get_dupes(specimen_id)
+
+### Combine data with metadata ----
+
+phenotypes_non_pool <- phenotype_data %>%
+  filter(!str_starts(specimen_id, "Pool")) %>%
+  inner_join(biospecimen_metadata, by = c("individual_id", "specimen_id")) %>%
+  inner_join(individual_metadata, by = "individual_id") %>%
+  select(individual_id, specimen_id, mouse_model, sex, age, tissue, phenotype, units, value)
+
+phenotypes_pool <- phenotype_data %>%
+  filter(str_starts(specimen_id, "Pool")) %>%
+  inner_join(pool_metadata, by = "specimen_id") %>%
+  select(individual_id, specimen_id, mouse_model, sex, age, tissue, phenotype, units, value)
+
+phenotypes <- phenotypes_non_pool %>%
+  bind_rows(phenotypes_pool)
+
+# Check none were lost
+nrow(phenotypes) == nrow(phenotype_data)
+
+# Combine models ----
+
+# TODO
+
+# Create a display name for phenotypes (with beta symbol instead of "beta") and one with units to display on Y-Axis:
+
+phenotypes <- phenotypes %>%
+  mutate(
+    phenotype_display = str_replace(phenotype, "Abeta", "A\u03B2"),
+    phenotype_units = glue::glue("{phenotype_display}\n({units})")
+  ) %>%
+  arrange(phenotype)
+
 # Save data ----
 
 usethis::use_data(phenotypes, overwrite = TRUE)
