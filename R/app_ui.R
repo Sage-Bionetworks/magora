f3893dbcbed176e26efffe4518606cd7b34159d3
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-04-01 11:56:58 -0400

    Position header above navbar

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,18 +10,19 @@ app_ui <- function(request) {
       Shiny.onInputChange("page_link", page)
       }')),
     golem_add_external_resources(),
+    mod_header_ui(),
     shiny::navbarPage(
       id = "page",
       title = NULL,
-      header = mod_header_ui(),
+      header = NULL,
       collapsible = TRUE,
       mod_start_ui(),
       mod_pathology_ui("pathology"),
-      shiny::navbarMenu(
-        title = "Gene Expression",
-        mod_gene_expression_volcano_ui("gene_expression_volcano"),
-        mod_gene_expression_heatmap_ui("gene_expression_heatmap")
-      ),
+      # shiny::navbarMenu(
+      #   title = "Gene Expression",
+      #   mod_gene_expression_volcano_ui("gene_expression_volcano"),
+      #   mod_gene_expression_heatmap_ui("gene_expression_heatmap")
+      # ),
       mod_nanostring_ui("nanostring"),
       mod_available_models_ui("available_models")
     )
--- R/mod_header.R
+++ R/mod_header.R
@@ -1,21 +1,21 @@
 #' Header UI
 #'
 mod_header_ui <- function() {
-    # shiny::div(
-    #   class = "container-fluid magora-header",
-    #   shiny::div(class = "magora-topbar"),
-    #   shiny::div(
-    #     class = "magora-brand",
-        shiny::tags$a(
-          # class = "magora-brand",
-          title = "Model AD Explorer",
-          shiny::tags$img(
-            src = "www/full_logo.svg",
-            width = "278px",
-            height = "100px",
-            alt = "Model AD Explorer"
-          )
+  shiny::div(
+    class = "container-fluid magora-header",
+    shiny::div(class = "magora-topbar"),
+    shiny::div(
+      class = "magora-brand",
+      shiny::tags$a(
+        class = "magora-brand",
+        title = "Model AD Explorer",
+        shiny::tags$img(
+          src = "www/full_logo.svg",
+          width = "278px",
+          height = "100px",
+          alt = "Model AD Explorer"
         )
-    #   )
-    # )
+      )
+    )
+  )
 }
