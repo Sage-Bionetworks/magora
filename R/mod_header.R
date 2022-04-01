0bfd1339c933056a7c58789c1271038979d0e874
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-04-01 12:29:07 -0400

    Clean up unused CSS classes

--- R/mod_header.R
+++ R/mod_header.R
@@ -2,12 +2,11 @@
 #'
 mod_header_ui <- function() {
   shiny::div(
-    class = "container-fluid magora-header",
+    class = "container-fluid",
     shiny::div(class = "magora-topbar"),
     shiny::div(
       class = "magora-brand",
       shiny::tags$a(
-        class = "magora-brand",
         title = "Model AD Explorer",
         shiny::tags$img(
           src = "www/full_logo.svg",
