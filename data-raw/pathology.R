0595805d5ce8122c3b6a86046e8d5980b48e0e15
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-05-30 11:59:31 -0400

    Add Trem2 model, migrate to list format for pathology

--- data-raw/pathology.R
+++ data-raw/pathology.R
@@ -15,10 +15,12 @@ source(here::here("data-raw", "check_latest_version.R"))
 
 # 5xFAD ----
 
+model <- "5xFAD"
+
 ## Download and read phenotype data ----
 
 # Read file that contains the phenotype name, synapse ID, field to use, and values of Stain to filter for
-phenotype_files <- read_csv(here::here("data-raw", "pathology", "5xFAD", "5xfad_data_sources.csv"))
+phenotype_files <- read_csv(here::here("data-raw", "pathology", model, glue::glue("{model}_data_sources.csv")))
 
 # Check that the latest version of all files is used
 walk2(phenotype_files[["syn_id"]], phenotype_files[["version"]], check_latest_version)
@@ -28,7 +30,7 @@ walk2(phenotype_files[["syn_id"]], phenotype_files[["version"]], check_latest_ve
 phenotype_distinct_files <- phenotype_files %>%
   distinct(syn_id, version)
 
-phenotype_paths <- map2(phenotype_distinct_files[["syn_id"]], phenotype_distinct_files[["version"]], ~ synapser::synGet(.x, version = .y, downloadLocation = here::here("data-raw", "pathology", "5xFAD"), ifcollision = "overwrite.local"))
+phenotype_paths <- map2(phenotype_distinct_files[["syn_id"]], phenotype_distinct_files[["version"]], ~ synapser::synGet(.x, version = .y, downloadLocation = here::here("data-raw", "pathology", model), ifcollision = "overwrite.local"))
 
 # Extract path and combine with phenotype_files df so that full path can be used, in case file name changes, rather than whatever is hardcoded in
 
@@ -80,7 +82,7 @@ biospecimen_version <- 9
 
 check_latest_version(biospecimen_id, biospecimen_version)
 
-biospecimen_metadata_path <- synGet(biospecimen_id, version = biospecimen_version, downloadLocation = here::here("data-raw", "pathology", "5xFAD"), ifcollision = "overwrite.local")
+biospecimen_metadata_path <- synGet(biospecimen_id, version = biospecimen_version, downloadLocation = here::here("data-raw", "pathology", model), ifcollision = "overwrite.local")
 
 biospecimen_metadata <- read_csv(biospecimen_metadata_path[["path"]]) %>%
   mutate(individualID = as.character(individualID)) %>%
@@ -119,7 +121,7 @@ individual_version <- 12
 
 check_latest_version(individual_id, individual_version)
 
-individual_metadata_path <- synGet(individual_id, version = individual_version, downloadLocation = here::here("data-raw", "pathology", "5xFAD"), ifcollision = "overwrite.local")
+individual_metadata_path <- synGet(individual_id, version = individual_version, downloadLocation = here::here("data-raw", "pathology", model), ifcollision = "overwrite.local")
 
 individual_metadata <- read_csv(individual_metadata_path[["path"]]) %>%
   mutate(individualID = as.character(individualID)) %>%
@@ -208,10 +210,12 @@ phenotypes_5xfad <- phenotype_data %>%
 
 # 3xTg ----
 
+model <- "3xTg-AD"
+
 ## Download and read  data ----
 
 # Read file that contains the phenotype name, synapse ID, field to use, and values of Stain to filter for
-phenotype_files <- read_csv(here::here("data-raw", "pathology", "3xTg", "3xTg_data_sources.csv"))
+phenotype_files <- read_csv(here::here("data-raw", "pathology", model, glue::glue("{model}_data_sources.csv")))
 
 # Check that the latest version of all files is used
 walk2(phenotype_files[["syn_id"]], phenotype_files[["version"]], check_latest_version)
@@ -221,7 +225,7 @@ walk2(phenotype_files[["syn_id"]], phenotype_files[["version"]], check_latest_ve
 phenotype_distinct_files <- phenotype_files %>%
   distinct(syn_id, version)
 
-phenotype_paths <- map2(phenotype_distinct_files[["syn_id"]], phenotype_distinct_files[["version"]], ~ synapser::synGet(.x, version = .y, downloadLocation = here::here("data-raw", "pathology", "3xTg"), ifcollision = "overwrite.local"))
+phenotype_paths <- map2(phenotype_distinct_files[["syn_id"]], phenotype_distinct_files[["version"]], ~ synapser::synGet(.x, version = .y, downloadLocation = here::here("data-raw", "pathology", model), ifcollision = "overwrite.local"))
 
 # Extract path and combine with phenotype_files df so that full path can be used, in case file name changes, rather than whatever is hardcoded in
 
@@ -283,7 +287,7 @@ biospecimen_version <- 3
 
 check_latest_version(biospecimen_id, biospecimen_version)
 
-biospecimen_metadata_path <- synGet(biospecimen_id, version = biospecimen_version, downloadLocation = here::here("data-raw", "pathology", "3xTg"), ifcollision = "overwrite.local")
+biospecimen_metadata_path <- synGet(biospecimen_id, version = biospecimen_version, downloadLocation = here::here("data-raw", "pathology", model), ifcollision = "overwrite.local")
 
 biospecimen_metadata <- read_csv(biospecimen_metadata_path[["path"]]) %>%
   mutate(individualID = as.character(individualID)) %>%
@@ -297,7 +301,7 @@ individual_version <- 4
 
 check_latest_version(individual_id, individual_version)
 
-individual_metadata_path <- synGet(individual_id, version = individual_version, downloadLocation = here::here("data-raw", "pathology", "3xTg"), ifcollision = "overwrite.local")
+individual_metadata_path <- synGet(individual_id, version = individual_version, downloadLocation = here::here("data-raw", "pathology", model), ifcollision = "overwrite.local")
 
 individual_metadata <- read_csv(individual_metadata_path[["path"]]) %>%
   mutate(individualID = as.character(individualID)) %>%
@@ -386,30 +390,210 @@ nrow(phenotypes_3xtg) == nrow(phenotype_data)
 phenotypes_3xtg <- phenotypes_3xtg %>%
   mutate(mouse_model = ifelse(mouse_model == "B6129F3", "B6129", mouse_model))
 
-# Combine models ----
+# Trem2-R47H_NSS ----
 
-pathology <- bind_rows(
-  phenotypes_5xfad %>%
-    mutate(mouse_model_group = "5xFAD"),
-  phenotypes_3xtg %>%
-    mutate(mouse_model_group = "3xTg-AD")
-) %>%
+model <- "Trem2-R47H_NSS"
+
+## Download and read phenotype data ----
+
+# Read file that contains the phenotype name, synapse ID, field to use, and values of Stain to filter for
+phenotype_files <- read_csv(here::here("data-raw", "pathology", model, glue::glue("{model}_data_sources.csv")))
+
+# Check that the latest version of all files is used
+walk2(phenotype_files[["syn_id"]], phenotype_files[["version"]], check_latest_version)
+
+# Download data - synapser checks the version locally and does not redownload if the version is up to date, so we can safely run all of this without worrying about redownloading.
+
+phenotype_distinct_files <- phenotype_files %>%
+  distinct(syn_id, version)
+
+phenotype_paths <- map2(phenotype_distinct_files[["syn_id"]], phenotype_distinct_files[["version"]], ~ synapser::synGet(.x, version = .y, downloadLocation = here::here("data-raw", "pathology", model), ifcollision = "overwrite.local"))
+
+# Extract path and combine with phenotype_files df so that full path can be used, in case file name changes, rather than whatever is hardcoded in
+
+phenotype_paths <- phenotype_paths %>%
+  map("path") %>%
+  map(~ tibble(file = .x))
+
+names(phenotype_paths) <- phenotype_distinct_files[["syn_id"]]
+
+phenotype_paths <- phenotype_paths %>%
+  bind_rows(.id = "syn_id")
+
+phenotype_files <- phenotype_files %>%
+  left_join(phenotype_paths, by = "syn_id", suffix = c("_hardcoded", ""))
+
+# Function to read in, filter + clean data, and return relevant fields
+read_clean_phenotype <- function(field, stain_filter, file) {
+  res <- readr::read_csv(file, na = c("N/A", ""))
+
+  res <- res %>%
+    janitor::clean_names()
+
+  if (!is.na(stain_filter)) {
+    res <- res %>%
+      dplyr::filter(stain == stain_filter)
+  }
+
+  res %>%
+    dplyr::select(tidyselect::all_of(c("individual_id", "specimen_id", value = janitor::make_clean_names(field)))) %>%
+    dplyr::mutate_all(as.character) %>%
+    janitor::remove_empty("rows")
+}
+
+# Read in data
+
+phenotype_data <- phenotype_files %>%
+  mutate(data = pmap(list(field, stain_filter, file), read_clean_phenotype)) %>%
+  select(syn_id, phenotype, units, data) %>%
+  unnest(cols = data) %>%
+  mutate(value = as.numeric(value)) %>%
+  filter(!is.na(value))
+
+## Metadata ----
+
+### Biospecimen metadata ----
+
+biospecimen_id <- "syn29568452"
+biospecimen_version <- 1
+
+check_latest_version(biospecimen_id, biospecimen_version)
+
+biospecimen_metadata_path <- synGet(biospecimen_id, version = biospecimen_version, downloadLocation = here::here("data-raw", "pathology", model), ifcollision = "overwrite.local")
+
+biospecimen_metadata <- read_csv(biospecimen_metadata_path[["path"]]) %>%
+  mutate(individualID = as.character(individualID)) %>%
+  clean_names() %>%
+  select(individual_id, specimen_id, tissue)
+
+#### Check metadata has correct tissue identifier ---
+
+correct_tissue_identifier <- tibble::tribble(
+  ~tissue, ~specimen_identifier,
+  "hippocampus", "h",
+  "cerebral cortex", "c",
+  "plasma", "p"
+)
+
+biospecimen_metadata %>%
   mutate(
-    mouse_model_group = as_factor(mouse_model_group),
-    mouse_model = fct_relevel(mouse_model, c("C57BL/6J", "5xFAD", "B6129", "3xTg-AD"))
+    specimen_id = tolower(specimen_id),
+    specimen_identifier = case_when(
+      str_detect(specimen_id, "c") ~ "c",
+      str_detect(specimen_id, "h") ~ "h",
+      str_detect(specimen_id, "p") ~ "p"
+    )
+  ) %>%
+  anti_join(correct_tissue_identifier, by = c("tissue", "specimen_identifier"))
+
+### Individual metadata ----
+
+individual_id <- "syn27147690"
+individual_version <- 2
+
+check_latest_version(individual_id, individual_version)
+
+individual_metadata_path <- synGet(individual_id, version = individual_version, downloadLocation = here::here("data-raw", "pathology", model), ifcollision = "overwrite.local")
+
+individual_metadata <- read_csv(individual_metadata_path[["path"]]) %>%
+  mutate(individualID = as.character(individualID)) %>%
+  clean_names() %>%
+  select(individual_id, sex, genotype, genotype_background, individual_common_genotype, age_death, age_death_units)
+
+### Check missing IDs ----
+
+# Check which IDs are missing from metadata
+phenotype_data %>%
+  anti_join(biospecimen_metadata, by = c("individual_id", "specimen_id")) %>%
+  distinct(individual_id, specimen_id)
+
+phenotype_data %>%
+  anti_join(individual_metadata, by = "individual_id") %>%
+  distinct(individual_id)
+
+## Clean data ----
+
+biospecimen_metadata <- biospecimen_metadata %>%
+  mutate(tissue = str_to_title(tissue))
+
+# Check that all age death units are "months"
+
+individual_metadata %>%
+  count(age_death_units) %>%
+  pull(age_death_units) == "months"
+
+# Check ages
+
+individual_metadata %>%
+  count(age_death)
+
+## Clean up factors etc
+
+individual_metadata <- individual_metadata %>%
+  mutate(
+    sex = str_to_title(sex),
+    sex = as_factor(sex),
+    age_factor = as_factor(age_death),
+    age_factor = fct_reorder(age_factor, age_death),
+    mouse_model = individual_common_genotype
+  ) %>%
+  select(-genotype, -genotype_background, -individual_common_genotype) %>%
+  rename(age = age_factor)
+
+# Recode mouse models
+
+individual_metadata <- individual_metadata %>%
+  mutate(
+    mouse_model = str_replace_all(mouse_model, "5XFAD", "5xFAD"),
+    mouse_model = str_replace_all(mouse_model, "C57BL6J", "C57BL/6J"),
+    mouse_model = as_factor(mouse_model)
   )
+
+### Combine data ----
+
+phenotypes_trem2_r47h_nss <- phenotype_data %>%
+  inner_join(biospecimen_metadata, by = c("individual_id", "specimen_id")) %>%
+  inner_join(individual_metadata, by = "individual_id") %>%
+  select(individual_id, specimen_id, mouse_model, sex, age, tissue, phenotype, units, value)
+
+# Combine models ----
+# Also set factor order, for plots (individually for each model)
+
+pathology <- list(
+  "5xFAD" = phenotypes_5xfad %>%
+    mutate(
+      mouse_model_group = "5xFAD",
+      mouse_model = fct_relevel(mouse_model, c(
+        "5xFAD", "C57BL/6J"
+      ))
+    ),
+  "3xTg-AD" = phenotypes_3xtg %>%
+    mutate(
+      mouse_model_group = "3xTg-AD",
+      mouse_model = fct_relevel(mouse_model, c(
+        "3xTg-AD", "B6129"
+      ))
+    ),
+  "Trem2-R47H_NSS" = phenotypes_trem2_r47h_nss %>%
+    mutate(
+      mouse_model_group = "Trem2-R47H_NSS",
+      mouse_model = fct_relevel(mouse_model, c(
+        "5xFADTrem2-R47H_NSS", "Trem2-R47H_NSS", "5xFAD", "C57BL/6J"
+      ))
+    )
+)
 
 # Create a display name for phenotypes (with beta symbol instead of "beta") and one with units to display on Y-Axis:
 
 pathology <- pathology %>%
-  mutate(
-    phenotype_display = str_replace(phenotype, "Abeta", "A\u03B2"),
-    phenotype_units = glue::glue("{phenotype_display}\n({units})")
-  ) %>%
-  arrange(phenotype)
-
-pathology <- pathology %>%
-  mutate(mouse_model = fct_relevel(mouse_model, c("C57BL/6J", "5xFAD", "B6129", "3xTg-AD")))
+  map(function(x) {
+    x %>%
+      mutate(
+        phenotype_display = str_replace(phenotype, "Abeta", "A\u03B2"),
+        phenotype_units = glue::glue("{phenotype_display}\n({units})")
+      ) %>%
+      arrange(phenotype)
+  })
 
 # Save data ----
 
@@ -417,7 +601,7 @@ usethis::use_data(pathology, overwrite = TRUE)
 
 # Separately save tissue available for each phenotype, for easily changing inputs available
 
-pathology_tissue <- split(pathology, pathology$phenotype) %>%
+pathology_tissue <- pathology %>%
   map(function(x) {
     distinct(x, tissue) %>%
       pull(tissue) %>%
@@ -426,9 +610,9 @@ pathology_tissue <- split(pathology, pathology$phenotype) %>%
 
 usethis::use_data(pathology_tissue, overwrite = TRUE)
 
-# Separately save mouse model (variant and control) for each group, for easily selecting just variant & getting both
+# Separately save mouse model (models and control) for each group, for easily selecting just model and getting all
 
-pathology_mouse_models <- split(pathology, pathology$mouse_model_group) %>%
+pathology_mouse_models <- pathology %>%
   map(function(x) {
     x %>%
       arrange(mouse_model) %>%
